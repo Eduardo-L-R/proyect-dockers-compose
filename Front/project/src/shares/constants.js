e9881b874daa95f2a6ee3c2683cef447336e0c35
Eduardo Leiva <eleiva@interside.cl>
2021-09-15 04:01:35 -0300

    v1.0.1

--- Front/project/src/shares/constants.js
+++ Front/project/src/shares/constants.js
@@ -1,3 +1,4 @@
 export const constants = {
-    endpoint : "http://localhost:5000/api/"
+    // endpoint : "http://localhost:5000/api/"
+    endpoint : "http://167.99.241.84:5000/api/" //production
 }
